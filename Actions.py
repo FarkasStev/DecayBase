f5c0821804a87aa9fea0eb49bcbecb65c7448473
Steven Farkas <farkas.stev@gmail.com>
2026-02-08 13:40:06 -0500

    Added basic structure to execute actions

--- Actions.py
+++ Actions.py
@@ -6,3 +6,15 @@ class Action(Enum):
     REFACTOR = 2
     ONBOARD_DEV = 3
     RUSH_DEADLINE = 4
+
+    def take_action(self, team):
+        if self.name == "ADD_FEATURE":
+            print("Adding a new feature")
+        elif self.name == "REFACTOR":
+            print("Refactoring the code")
+        elif self.name == "ONBOARD_DEV":
+            print("Onboarding a new developer")
+        elif self.name == "RUSH_DEADLINE":
+            print("Rushing to meet a deadline")
+
+        return 10
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+import Actions
+import config
+from DeveloperTeam import DeveloperTeam
+
+
 def main():
-    money = 100
-    cost = 10
+    money = config.STARTING_MONEY
     sprint_number = 0
+    team = DeveloperTeam(config.STARTING_TEAM_SIZE)
     while money > 0:
         print("Money:", money)
         print("Choose an option")
+        for action in Actions.Action:
+            print(f"{action.value}: {action.name}")
         option = input()
-        print("You chose: ", option)
+        action = Actions.Action(int(option))
+        cost = action.take_action(team)
         money -= cost
         sprint_number += 1
     pass
